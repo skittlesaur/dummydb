6ad698bf503e95dfeba683bcfe9fd57ba23dda65
baraa <baraa0ah@gmail.com>
2022-05-14 15:03:21 +0200

    initialized server files

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.json({
+        title: "Dummy Database API",
+        version: process.env.npm_package_version
+    })
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
