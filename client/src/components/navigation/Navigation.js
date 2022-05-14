d557da7bfe47c6d1e70f3f073bc5528380fc5cd5
baraa <baraa0ah@gmail.com>
2022-05-14 20:58:03 +0200

    updated generate button href

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = () => {
                 <Link className={'btn2'} to={'/releases'}>Releases</Link>
                 <Link className={'btn2'} to={'/contribute'}>Contribute</Link>
             </nav>
-            <Link to={'/'} className={`btn1 btn1-b`}>Generate</Link>
+            <Link to={'/generate'} className={`btn1 btn1-b`}>Generate</Link>
         </div>
     );
 }
