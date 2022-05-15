232a954da3a5625e5369acb49290654922876284
baraa <baraa0ah@gmail.com>
2022-05-15 10:59:51 +0200

    Added type selector in generate page

--- client/src/pages/generate/Generate.js
+++ client/src/pages/generate/Generate.js
@@ -1,14 +1,16 @@
 import styles from './generate.module.css';
 import {useState} from "react";
 import DeleteImage from '../../shared/assets/delete.png';
+import Type from "../../components/popup/data-type/Type";
 
 const Generate = () => {
     const [data, setData] = useState([
-        {field: "", type: ""}
+        {field: "", type: "Type"}
     ]);
+    const [typeSelect, setTypeSelect] = useState({visible: false});
 
     const handleAddField = () => {
-        const field = {field: '', type: ''};
+        const field = {field: '', type: 'Type'};
         const arr = [...data, field];
         setData(arr);
     }
@@ -26,26 +28,33 @@ const Generate = () => {
         setData(arr);
     }
 
+    const handleTypeSelection = (i) => {
+        setTypeSelect({visible: true, data: data, i: i});
+    }
+
     return (
-        <div className={styles['wrapper']}>
-            <h2>Database Schema</h2>
-            <div className={styles['user-schema']}>
-                <div className={styles['schema-item']}>
-                    <div>Field Name</div>
-                    <div>Type</div>
+        <>
+            {typeSelect?.visible && <Type typeSelect={typeSelect} setTypeSelect={setTypeSelect}/>}
+            <div className={styles['wrapper']}>
+                <h2>Database Schema</h2>
+                <div className={styles['user-schema']}>
+                    <div className={styles['schema-item']}>
+                        <div>Field Name</div>
+                        <div>Type</div>
+                    </div>
+                    {data.map((item, i) =>
+                        <div key={i} className={styles['schema-item']}>
+                            <input onChange={(e) => handleChange(e, i)} name={'field'}
+                                   value={item.field} placeholder={'Field Name'}/>
+                            <div onClick={() => handleTypeSelection(i)} className={styles['type']}
+                                 style={{color: item.type !== "Type" ? "white" : ""}}>{item.type}</div>
+                            <img onClick={() => handleDelete(i)} src={DeleteImage} className={styles['delete']}
+                                 alt={'Delete'}/>
+                        </div>)}
                 </div>
-                {data.map((item, i) =>
-                    <div key={i} className={styles['schema-item']}>
-                        <input onChange={(e) => handleChange(e, i)} name={'field'}
-                               value={item.field} placeholder={'Field Name'}/>
-                        <input disabled={true} className={styles['type']} name={'type'} value={item.type}
-                               placeholder={'Type'}/>
-                        <img onClick={() => handleDelete(i)} src={DeleteImage} className={styles['delete']}
-                             alt={'Delete'}/>
-                    </div>)}
+                <div onClick={handleAddField} className={`btn1 btn1-b ${styles[`add-field`]}`}>Add Field</div>
             </div>
-            <div onClick={handleAddField} className={`btn1 btn1-b ${styles[`add-field`]}`}>Add Field</div>
-        </div>
+        </>
     );
 }
 
